8a6767aba881f4b925a61624deb92ddec951eabe
Hackerberger <hechenberger.l00@htl-ottakring.ac.at>
2020-12-05 13:25:42 +0100

    test with app.js changes

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+//app.use('/', indexRouter);
 
 module.exports = app;
