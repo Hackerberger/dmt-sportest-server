ef4ee3003f042c9d8e9795a7687a9f97add7d07c
Hackerberger <luckyluke4002@gmail.com>
2021-04-16 11:57:50 +0200

    Umstieg auf neues Vue

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,16 +14,12 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5ccccb1d9c440bb387e165c73f6a3cf.js"
+  "/precache-manifest.9d44924c60c9e80601580d5ae4574b9c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dmt-sportest"});
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+workbox.core.skipWaiting();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
